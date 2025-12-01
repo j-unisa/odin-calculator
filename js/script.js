236b0740a85d3e7de720f96976274b27e7bc4aba
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-12-01 15:50:23 -0800

    feat: implement basic arithmetic functions
    - add add, subtract, multiply, and divide functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// External JavaScript
+// Add
+function add(x, y)
+{
+    return x + y;
+}
+
+// Subtract
+function subtract(x, y)
+{
+    return x - y;
+}
+
+// Multiply
+function multiply(x, y)
+{
+    return x * y;
+}
+
+// Divide
+function divide(x, y)
+{
+    return x / y;
+}
