6eb4b8007bc6c53fdc20cff0efdc949ddfd23c87
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-12-01 16:09:48 -0800

    feat: add operate function for calculations
    - implement operate to route operations to correct arithmetic functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+let num1;
+let operator;
+let num2;
+
+function operate(num1, operator, num2)
+{
+    switch(operator)
+    {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
 // Add
 function add(x, y)
 {
