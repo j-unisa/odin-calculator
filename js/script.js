c921cd428d010ff80fc4b864c0fa0d834cf1fa34
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-12-03 11:08:39 -0800

    feat: add operator check and num1 logging
    - add operators string for operator detection
    - add logic to store num1 when number is clicked
    - add operator check using includes() and log operator

--- js/script.js
+++ js/script.js
@@ -8,8 +8,22 @@ const buttons = document.querySelector("#buttons");
 buttons.addEventListener("click", function(e)
     {
         display.textContent += e.target.textContent;
+        const operators = "+-*/";
+
         // Store first number in num1
+        if ((typeof +e.target.textContent == "number") && !Number.isNaN(+e.target.textContent))
+        {
+            // TODO: Figure out how to differentiate between first number and second number
+            num1 = +e.target.textContent;
+            console.log(+e.target.textContent);
+        }
         // Store operator in operator
+        else if (operators.includes(e.target.textContent))
+        {
+            // Replace with code to store operator in operator variable
+            console.log("I'm an operator");
+        }
+
         // Store second number in num2
         // Call operate() when = button is clicked
     });
